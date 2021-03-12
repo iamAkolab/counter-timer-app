d89562d07283ab4e6679210f9203de8e9ade180e
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-03-12 23:58:13 +0100

    added fan...........

--- countDown.py
+++ countDown.py
@@ -11,20 +11,27 @@ def quit(*args):
     root.destroy()
 
 def cant_wait():
+    #Get the time remianing until the event
     timeLeft = endTime - datetime.datetime.now()
-    time = timeLeft - datetime.timedelta(microseconds=timeLeft.microseconds)
 
+    #remove the microseconds part
+    time = timeLeft - datetime.timedelta(microseconds=timeLeft.microseconds)
+    
+    #show the time left
     txt.set(timeLeft)
 
+    #Triggers the countdown after 1000ms
     root.after(1000,cant_wait)
 
+#Use tkinter lib for showing the clock
 root = Tk()
 root.attributes("-fullscreen", False)
 root.configure(background="black")
 root.bind("x",quit)
 root.after(1000,cant_wait)
 
-endTime = datetime.datetime(2017,9,30,0)
+#   Set the end date and time for the countdown
+endTime = datetime.datetime(2021,9,30,0)
 fnt = font.Font(family = "Helvetica", size = 90, weight = "bold")
 
 txt = StringVar()
